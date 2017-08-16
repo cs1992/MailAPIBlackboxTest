c775b4b860b290f74c85f08dde2a9303d584362e
NAVER <NAVER@AL10000143>
2017-08-16 10:05:30 +0900

    test.js

--- mail_api_blackbox_test_2/src/com/mail/blackbox/option/model/OptionSmtpGetParam.java
+++
@@ -1,7 +0,0 @@
-package com.mail.blackbox.option.model;
-
-import com.mail.blackbox.model.Param;
-
-public class OptionSmtpGetParam extends Param  {
-
-}
--- mail_api_blackbox_test_2/src/com/mail/blackbox/option/model/OptionSpamGetParam.java
+++
@@ -1,7 +0,0 @@
-package com.mail.blackbox.option.model;
-
-import com.mail.blackbox.model.Param;
-
-public class OptionSpamGetParam extends Param  {
-
-}
